9b8666f2526b5a15813fb26f251bfe51258ee9ea
siv727 <143003088+siv727@users.noreply.github.com>
2023-09-29 12:51:45 +0800

    Update temperatureconverter.c

--- temperatureconverter.c
+++ temperatureconverter.c
@@ -51,32 +51,32 @@ int main(){
 //CONVERSION
     switch(tempconvertfrom){ 
         case 1: // KELVIN 
-            switch(tempconvertto){
-        		case 5:
-        			printf("\nThe temperature is %.2f°C", kelvinToCelsius(temp));
+		if(tempconvertto == 5){
+        		printf("\nThe temperature is %.2f°C", kelvinToCelsius(temp));
+            		break;	
+		}
+		else if(tempconvertto == 6){
+			printf("\nThe temperature is %.2f°C", kelvinToFahrenheit(temp));
             		break;
-            	case 6:
-        			printf("\nThe temperature is %.2f°C", kelvinToFahrenheit(temp));
-            		break;
-			} 
+		}
         case 2: // CELSIUS
-        	switch(tempconvertto){
-        		case 4:
-        			printf("\nThe temperature is %.2f°C", celsiusToKelvin(temp));
+        	if(tempconvertto == 4){
+        		printf("\nThe temperature is %.2f°C", celsiusToKelvin(temp));
+            		break;	
+		}
+		else if(tempconvertto == 6){
+			printf("\nThe temperature is %.2f°C", celsiusToFahrenheit(temp));
             		break;
-            	case 6:
-        			printf("\nThe temperature is %.2f°C", celsiusToFahrenheit(temp));
-            		break;
-			} 
+        	}
         case 3: //FAHRENHEIT
-        	switch(tempconvertto){
-        		case 4:
-        			printf("\nThe temperature is %.2f°C", fahrenheitToKelvin(temp));
+        	if(tempconvertto == 4){
+        		printf("\nThe temperature is %.2f°C", fahrenheitToKelvin(temp));
+            		break;	
+		}
+		else if(tempconvertto == 5){
+			printf("\nThe temperature is %.2f°C", fahrenheitToCelsius(temp));
             		break;
-            	case 5:
-        			printf("\nThe temperature is %.2f°C", fahrenheitToCelsius(temp));
-            		break;
-			} 
+        	}
     }
 
     return 0;
@@ -85,42 +85,42 @@ int main(){
 
 //FUNCTION SPECIFICATIONS
 float fahrenheitToKelvin(float temp){
-	printf("Enter the temperature in Fahrenheit: ");
+    printf("Enter the temperature in Fahrenheit: ");
     scanf("%f", &temp);
     float kTemp = ((temp - 32)*5)/9 + KELVIN;
     return kTemp;
 }
 
 float fahrenheitToCelsius(float temp){
-	printf("Enter the temperature in Fahrenheit: ");
+    printf("Enter the temperature in Fahrenheit: ");
     scanf("%f", &temp);
     float cTemp = ((temp - 32)*5)/9;
     return cTemp;
 }
 
 float celsiusToKelvin(float temp){
-	printf("Enter the temperature in Celsius: ");
+    printf("Enter the temperature in Celsius: ");
     scanf("%f", &temp);
     float kTemp = temp + KELVIN;
     return kTemp;
 }
 
 float celsiusToFahrenheit(float temp){
-	printf("Enter the temperature in Celsius: ");
+    printf("Enter the temperature in Celsius: ");
     scanf("%f", &temp);
     float fTemp = (temp * 9/5) + 32;
     return fTemp;
 }
 
 float kelvinToCelsius(float temp){
-	printf("Enter the temperature in Kelvin: ");
+    printf("Enter the temperature in Kelvin: ");
     scanf("%f", &temp);
     float cTemp = temp - KELVIN;
     return cTemp;
 }
 
 float kelvinToFahrenheit(float temp){
-	printf("Enter the temperature in Kelvin: ");
+    printf("Enter the temperature in Kelvin: ");
     scanf("%f", &temp);
     float cTemp = ((temp - KELVIN)*9)/5 + 32;
     return cTemp;
